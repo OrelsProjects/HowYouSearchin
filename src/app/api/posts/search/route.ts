ae9cb54ebdddb2354188d000d9295ac638a32ce1
orelsprojects <orelsmail@gmail.com>
2025-01-14 20:44:45 +0200

    more improvements

--- src/app/api/posts/search/route.ts
+++ src/app/api/posts/search/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 
-import { searchPosts } from "@/lib/post/utils/utils";
+import { searchPosts } from "@/lib/post/utils";
 import { NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
